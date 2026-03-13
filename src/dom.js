4012ae0b822ad870a6040655659cf98707b0a806
tanzim <syedtanzimwajih@gmail.com>
2026-03-13 23:56:26 +0530

    feat: add todoCard method

--- src/dom.js
+++ src/dom.js
@@ -8,4 +8,29 @@ class DomController {
 
         return projectCard;
     }
+
+    todoCard(title, description, date, priority) {
+        const todo = document.createElement('div');
+        todo.className = 'todo';
+
+        const todoTitle = document.createElement('h3');
+        todoTitle.textContent = title;
+
+        const todoDescription = document.createElement('p');
+        todoDescription.textContent = description;
+
+        const todoDate = document.createElement('p');
+        todoDate.textContent = date;
+
+        const todoPriority = document.createElement('p');
+        todoPriority.textContent = priority;
+
+        todo.appendChild(todoTitle);
+        todo.appendChild(todoDescription);
+        todo.appendChild(todoDate);
+        todo.appendChild(todoPriority);
+
+        return todo;
+    }
+
 }
